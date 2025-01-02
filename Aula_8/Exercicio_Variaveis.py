c2654bff72f5452975fccafffb1f2a2bc4570d03
unknown <zeinitrudesti20@gmail.com>
2025-01-01 22:06:40 -0300

    ano novo, vida nova

--- Aula_8/Exercicio_Variaveis.py
+++ Aula_8/Exercicio_Variaveis.py
@@ -4,6 +4,7 @@ idade = 2
 ano_nascimento = 1999
 maior_de_idade = idade >= 18
 altura_metros = 1.80
+ano_novo = "2025"
 
 print("Nome:", nome)
 print('Sobrenome:', sobrenome)
@@ -11,3 +12,6 @@ print('idade:', idade)
 print('Ano de Nascimento:', ano_nascimento)
 print('É maior de idade?', maior_de_idade)
 print('Altura em metros:', altura_metros)
+
+
+print("feliz", ano_novo, "pessoal")
