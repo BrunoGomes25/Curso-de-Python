10eff61a79b049ac419407dda6aaa212b8afc446
unknown <zeinitrudesti20@gmail.com>
2025-01-02 21:26:27 -0300

    last

---
+++ Aula_9/Opera_arit.py
@@ -0,0 +1,16 @@
+adicao = (10 + 10)
+print("adição =", adicao)
+subtracao = (10 - 5)
+print("subtração =", subtracao)
+multiplicacao = (10 * 10)
+print("multiplicação =", multiplicacao)
+divisao = (10 / 2.2)
+print("divisão =", divisao)
+divisao_inteira = (10 // 2.2)
+print("divisão inteira =" , divisao_inteira)
+# a divisao inteira ignora as casas decimais
+exponenciacao = (2 ** 10)
+print("exponenciação =", exponenciacao)
+modulo = (55 % 2) # resto da divisão
+print("modulo =", modulo)
+
